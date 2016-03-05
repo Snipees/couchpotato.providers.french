568cf882f74be70bceed726a5103f9e2fd96930d
Snipees <Snipees@users.noreply.github.com>
2016-03-05 12:10:55 +0100

    update to t411.ch

--- t411/main.py
+++ t411/main.py
@@ -18,12 +18,12 @@ log = CPLog(__name__)
 class t411(TorrentProvider, MovieProvider):
 
     urls = {
-        'test' : 'https://t411.in',
-        'login' : 'https://api.t411.in/auth',
-        'login_check': 'https://api.t411.in/torrents/top/100',
-        'detail' : 'https://www.t411.in/torrents/?id=%s',
-        'search' : 'https://api.t411.in/torrents/search/%s?limit=200&cid=631',
-        'download' : 'https://api.t411.in/torrents/download/%s',
+        'test' : 'https://t411.ch',
+        'login' : 'https://api.t411.ch/auth',
+        'login_check': 'https://api.t411.ch/torrents/top/100',
+        'detail' : 'https://www.t411.ch/torrents/?id=%s',
+        'search' : 'https://api.t411.ch/torrents/search/%s?limit=200&cid=631',
+        'download' : 'https://api.t411.ch/torrents/download/%s',
     }
 
     cat_ids = [
